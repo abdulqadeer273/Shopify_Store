48225b04bb557208bf1ace65299af11688d6024e
abdulqadeer273 <rq5047772@gmail.com>
2025-02-01 00:14:30 +0500

    SS-2 whatsapp integration done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Card, Navbar, Nav, Container } from 'react-bootstrap';
+import { FaWhatsapp } from 'react-icons/fa';
 import './App.css';
 import pimage1 from './assets/images/pimage1.jpg';
 import pimage2 from './assets/images/pimage2.png';
@@ -228,6 +229,30 @@ function App() {
             <div className="footer-bar mt-4">© 2023 Arkab</div>
           </div>
       </footer>
+
+      {/* WhatsApp Icon */}
+      <a
+        href="https://wa.me/923317726261"
+        className="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          position: 'fixed',
+          width: '50px',
+          height: '50px',
+          bottom: '40px',
+          right: '40px',
+          backgroundColor: '#25d366',
+          color: '#FFF',
+          borderRadius: '50px',
+          textAlign: 'center',
+          fontSize: '30px',
+          boxShadow: '2px 2px 3px #999',
+          zIndex: '1000'
+        }}
+      >
+        <FaWhatsapp />
+      </a>
     </div>
   );
 }
